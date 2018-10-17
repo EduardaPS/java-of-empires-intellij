867c6463bc78856e2101b6eebab11fa7f36213c8
Eduarda Passig <dudynha.passigs@gmail.com>
2018-10-17 08:05:19 -0300

    criando a classe pai

--- src/main/java/personagens/Aldeao.java
+++ src/main/java/personagens/Aldeao.java
@@ -7,28 +7,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.net.URL;
 
-public class Aldeao {
-
-    private JPanel pai;  // referência ao JPanel onde será desenhado o Aldeao
-    private Image icone; // imagem do Aldeao
-    private String imagemNome; // nome da imagem do Aldeao
-    private int largura; // largura da imagem
-    private int altura;  // altura da imagem
-    private int posX;    // posição X no JPanel onde a imagem será desenhada
-    private int posY;    // posição Y no JPanel onde a imagem será desenhada
-    private boolean atacando; // indica se está atacando ou não
+public class Aldeao extends Personagem {
 
     public Aldeao(JPanel p, String img, int x, int y) {
-
-        this.imagemNome = img;
-        this.icone = this.carregarImagem(this.imagemNome);
-        this.largura = this.icone.getWidth(null);
-        this.altura = this.icone.getHeight(null);
-
-        this.pai = p;
-        this.posX = x;
-        this.posY = y;
-        this.atacando = false;
+        super(p, img, x, y);
     }
 
 
--- src/main/java/personagens/Personagem.java
+++ src/main/java/personagens/Personagem.java
@@ -1,11 +1,29 @@
 package personagens;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Personagem {
 
     private JPanel pai;  // referência ao JPanel onde será desenhado o Aldeao
+    private Image icone; // imagem do Aldeao
+    private String imagemNome; // nome da imagem do Aldeao
     private int largura; // largura da imagem
     private int altura;  // altura da imagem
     private int posX;    // posição X no JPanel onde a imagem será desenhada
     private int posY;    // posição Y no JPanel onde a imagem será desenhada
     private boolean atacando; // indica se está atacando ou não
+
+    public Personagem(JPanel p, String img, int x, int y) {
+
+        this.imagemNome = img;
+        this.icone = this.carregarImagem(this.imagemNome);
+        this.largura = this.icone.getWidth(null);
+        this.altura = this.icone.getHeight(null);
+
+        this.pai = p;
+        this.posX = x;
+        this.posY = y;
+        this.atacando = false;
+    }
 }
