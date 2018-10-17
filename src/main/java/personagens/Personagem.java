40304725fbff4314c0a7c8172a4dfa9b41ad52e9
Eduarda Passig <dudynha.passigs@gmail.com>
2018-10-17 07:58:05 -0300

    criando a classe pai

---
+++ src/main/java/personagens/Personagem.java
@@ -0,0 +1,11 @@
+package personagens;
+
+public class Personagem {
+
+    private JPanel pai;  // referência ao JPanel onde será desenhado o Aldeao
+    private int largura; // largura da imagem
+    private int altura;  // altura da imagem
+    private int posX;    // posição X no JPanel onde a imagem será desenhada
+    private int posY;    // posição Y no JPanel onde a imagem será desenhada
+    private boolean atacando; // indica se está atacando ou não
+}
